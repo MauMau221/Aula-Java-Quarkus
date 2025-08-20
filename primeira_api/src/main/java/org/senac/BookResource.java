c7c186474633c434ca8388aed78c4a584558e699
MauMau221 <mauriciobgsp@gmail.com>
2025-08-19 21:22:46 -0300

    rotas de APIs

---
+++ primeira_api/src/main/java/org/senac/BookResource.java
@@ -0,0 +1,34 @@
+package org.senac;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+@Path("/books")
+public class BookResource {
+
+    @GET
+    public Response getAll() {
+        return Response.ok(Book.listAll()).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getById(@PathParam("id") int id) {
+        Book entity = Book.findById(id);
+        if(entity == null){
+            return Response.status(404).build();
+        }
+        return Response.ok(entity).build();
+    }
+
+    @POST
+    @Transactional // Serve para indicar que será feito alguma operação relacinada ao banco de dados
+    public Response insert(Book book) {
+        Book.persist(book); //insere novo livro
+        return Response.status(201).entity(book).build();
+    }
+}
--- primeira_api/src/main/java/org/senac/GreetingResource.java
+++ primeira_api/src/main/java/org/senac/GreetingResource.java
@@ -3,6 +3,7 @@ package org.senac;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.awt.*;
 import java.util.List;
@@ -32,7 +33,7 @@ public class GreetingResource {
     }
 
     @POST
-    @Transactional
+    @Transactional // Serve para indicar que será feito alguma operação relacinada ao banco de dados
     @Consumes(MediaType.APPLICATION_JSON)
     public void insertEntity(MyEntity entity){
         MyEntity.persist(entity);
@@ -40,8 +41,20 @@ public class GreetingResource {
 
     @DELETE
     @Path("{id}")
-    @Transactional
+    @Transactional // Serve para indicar que será feito alguma operação relacinada ao banco de dados
     public void delete(@PathParam("id") int id){
         MyEntity.deleteById(id);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional // Serve para indicar que será feito alguma operação relacinada ao banco de dados
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateEntity(MyEntity entity, @PathParam("id") int id) {
+        MyEntity entity1 = MyEntity.findById(id);
+        if(entity1 == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(entity1).build();
     }
 }
