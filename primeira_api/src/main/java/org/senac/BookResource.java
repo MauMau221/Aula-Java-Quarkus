42fbedffa334429d195759b6b4731ae73d62cf97
MauMau221 <mauriciobgsp@gmail.com>
2025-08-26 20:16:29 -0300

    search em java

--- primeira_api/src/main/java/org/senac/BookResource.java
+++ primeira_api/src/main/java/org/senac/BookResource.java
@@ -1,10 +1,11 @@
 package org.senac;
 
+import io.quarkus.panache.common.Sort;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
-import java.util.ArrayList;
+import java.util.Set;
 
 @Path("/books")
 public class BookResource {
@@ -18,7 +19,7 @@ public class BookResource {
     @Path("/{id}")
     public Response getById(@PathParam("id") int id) {
         Book entity = Book.findById(id);
-        if(entity == null){
+        if (entity == null) {
             return Response.status(404).build();
         }
         return Response.ok(entity).build();
@@ -26,10 +27,31 @@ public class BookResource {
 
     @GET
     @Path("/search")
-    public Response search(@QueryParam("title") String title, //Pesquisa
-                           @QueryParam("sort") String sort, // ordenação
-                           @QueryParam("direction") String direction){
-        var books = Book.find("titulo = ?1", title);
+    public Response search(
+            @QueryParam("q") String q,
+            @QueryParam("sort") @DefaultValue("id") String sort,
+            @QueryParam("direction") @DefaultValue("asc") String direction,
+            @QueryParam("page") @DefaultValue("0") int page,
+            @QueryParam("size") @DefaultValue("10") int size
+    ) {
+        Set<String> allowed = Set.of("id", "titulo", "autor", "editora", "anoLancamento", "estaDisponivel");
+        if (!allowed.contains(sort))
+            sort = "id";
+
+        Sort sortObj = Sort.by(
+                sort,
+                "desc".equalsIgnoreCase(direction) ? Sort.Direction.Descending : Sort.Direction.Ascending
+                );
+
+        int effectivePage = page <= 1 ? 0 : page - 1;
+
+        var query = (q == null || q.isBlank()
+                ? Book.findAll(sortObj)
+                :Book.find("lower(titulo) like ?1 or lower(autor) like ?1",
+                sortObj,
+                "%" + q.toLowerCase() + "%"));
+
+        var books = query.page(effectivePage, size).list();
         return Response.ok(books).build();
     }
 
@@ -52,8 +74,8 @@ public class BookResource {
     @Path("/{id}")
     @Transactional
     public Response update(@PathParam("id") int id, Book newbook) {
-        Book  entity = Book.findById(id);
-        if(entity == null){
+        Book entity = Book.findById(id);
+        if (entity == null) {
             return Response.status(404).build();
         }
         entity.titulo = newbook.titulo;
--- primeira_api/src/main/resources/import.sql
+++ primeira_api/src/main/resources/import.sql
@@ -4,3 +4,9 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+
+insert into book (id, titulo, autor, editora, anoLancamento, estaDisponivel) values (1, 'Harry', 'Potter', 'Sei la', 1998, true);
+insert into book (id, titulo, autor, editora, anoLancamento, estaDisponivel) values (2, 'Homem de Ferro', 'Marvel', 'Marvel', 1990, true);
+insert into book (id, titulo, autor, editora, anoLancamento, estaDisponivel) values (3, 'O caçador de pipas', 'Kabu', 'Raiassu', 1998, true);
+insert into book (id, titulo, autor, editora, anoLancamento, estaDisponivel) values (4, 'O poço', 'ele lá ', 'alguma', 1198, true);
